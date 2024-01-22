7648f5159127da8c40a752e7be07a7d607aeb1f4
Prabesh Magar <Prabesh Magar>
2024-01-22 21:59:55 +0545

    varables

---
+++ git.md
@@ -0,0 +1,9 @@
+initial project setup
+steps :
+1. git init
+2. git remote add origin<githublink>
+3. To check step 2 appy
+      - git remot -v
+
+<-- day to day -->
+1. git status
---
+++ index.js
@@ -0,0 +1,32 @@
+var fullName = "Prabesh Magar";
+let address = "putalisadak";
+const role = "admin";
+
+
+
+// Decleration and initialization of variables
+
+let size; //declaration
+size = "xl"; //initialization
+size = "xxl"; //initialization
+
+let age = 20;
+console.log(age);
+
+let length
+console.log(length)
+
+
+
+// array
+
+let products =  ["mous","keyboard","watch"]
+console.log(products);
+
+products[0] = "mouse";
+
+
+
+let colors = ["blu","yellow","red","green","crimsion","purple","orange"]
+colors[0] = "blue";
+console.log(colors);
